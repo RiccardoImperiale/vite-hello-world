09b03ddf2edc636bc6fafba52d286fb5f04eba55
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-20 13:52:54 +0100

    add simple style

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,30 @@
 export default {
   data() {
     return {
-      message: 'Hello Vite, hi Vue!!'
+      viteMessage: 'Hello Vite!!',
+      vueMessage: 'Hi Vue!!'
     }
   }
 }
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <div class="messages">
+    <h2>{{ viteMessage }}</h2>
+    <h2>{{ vueMessage }}</h2>
+  </div>
 </template>
 
-<style></style>
+<style>
+h2 {
+  font-size: 3rem;
+}
+
+h2:first-child {
+  color: #737BFF;
+}
+
+h2:last-child {
+  color: #43D392;
+}
+</style>
